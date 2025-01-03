7bbf7693b8a0604f1d81cb8691d84e2da02a62f4
Mamé Azad <mazad.contractor@libertyglobal.com>
2025-01-03 20:45:57 +0100

    add terraform

---
+++ terraform-frontend/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "react_frontend" {
+  bucket = var.bucket_name
+  
+}
+
+resource "aws_s3_bucket_policy" "react_frontend_policy" {
+  bucket = aws_s3_bucket.react_frontend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.react_frontend.arn}/*"
+      }
+    ]
+  })
+}
---
+++ terraform-frontend/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "AWS Region"
+  default = "eu-west-3"
+}
+
+variable "bucket_name" {
+  description = "The name of the s3 bucket"
+  default = "movie_react_app_mame_azad"
+}
