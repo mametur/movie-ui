a3b31a3258d162c9ac214381955f970c54d05d8f
Mamé Azad <mazad.contractor@libertyglobal.com>
2025-01-04 15:19:38 +0100

    fixed terraform files

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "react_frontend" {
   bucket = var.bucket_name
-  
+
 }
 
 resource "aws_s3_bucket_policy" "react_frontend_policy" {
@@ -14,10 +14,10 @@ resource "aws_s3_bucket_policy" "react_frontend_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
+        Effect    = "Allow"
         Principal = "*"
-        Action = "s3:GetObject"
-        Resource = "${aws_s3_bucket.react_frontend.arn}/*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.react_frontend.arn}/*"
       }
     ]
   })
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "region" {
   description = "AWS Region"
-  default = "eu-west-3"
+  default     = "eu-west-3"
 }
 
 variable "bucket_name" {
   description = "The name of the s3 bucket"
-  default = "movie_react_app_mame_azad"
+  default     = "movie_react_app_mame_azad"
 }
